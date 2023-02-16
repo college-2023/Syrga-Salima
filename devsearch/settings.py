2dd7783f03786deaab07441c87e9148157ac76af
csc2023 <123530301+csc2023@users.noreply.github.com>
2023-02-16 15:30:17 +0600

    Update settings.py

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-w2@4fvi69+$!568sea!=je$lqr70l!da0r-3bsczm4wye$+c@v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'devsearch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres2',
+        'USER': 'postgres2',
+        'PASSWORD': 'postgres2',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
