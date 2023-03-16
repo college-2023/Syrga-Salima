5f2e601f3d20ccab0e99a2730bfa9878197b9be2
college-2023 <123530301+college-2023@users.noreply.github.com>
2023-03-16 15:06:13 +0600

    Update settings.py

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-w2@4fvi69+$!568sea!=je$lqr70l!da0r-3bsczm4wye$+c@v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,9 +120,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = BASE_DIR / 'static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
