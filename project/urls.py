c12ddfe9535bb5e6d63470df411fb23d26fdaa8a
college-2023 <123530301+college-2023@users.noreply.github.com>
2023-04-19 11:58:27 +0600

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -6,5 +6,11 @@ urlpatterns = [
     path('logoutUser/', views.logoutUser, name = 'logoutUser'),
     path('registerUser/', views.registerUser , name = 'registerUser'),
     path('', views.main, name='main'),
+    path('bov', views.bov, name='bov'),
+    path('leo', views.leo, name='leo'),
+    path('lin', views.lin, name='lin'),
+    path('mic', views.mic, name='mic'),
+    path('tob', views.tob, name='tob'),
+    
     
 ]
